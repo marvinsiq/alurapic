06146d3f9c37e26e76c2a13c87e7fe6023db8cd4
Marcus Siqueira <marcus.siqueira@serpro.gov.br>
2020-01-28 18:17:25 -0300

    Tipando a API

--- web/src/app/photos/photo/photo.service.ts
+++ web/src/app/photos/photo/photo.service.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Photo } from './photo';
+
+const API = "http://localhost:3000";
 
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
 
     constructor(private http: HttpClient) { }
 
-    listFromUser(userName: string): Observable<Object[]> {
+    listFromUser(userName: string): Observable<Photo[]> {
 
         return this.http
-            .get<Object[]>(`http://localhost:3000/${userName}/photos`);
+            .get<Photo[]>(`${API}/${userName}/photos`);
     }
 
 }
