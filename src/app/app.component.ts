2d0ce82fa95cdbf30df5114527d5b03090d3386f
Marcus Siqueira <marcus.siqueira@serpro.gov.br>
2020-01-28 17:23:06 -0300

    Adicionando o bootstrap. Criando o componente photo. Criando o módulo photos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+
+  photos = [
+    {
+      url: 'https://i.imgur.com/dpS6gvq.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'https://www.brasileirosemushuaia.com.br/media/catalog/product/cache/1/image/700x460/17f82f742ffe127f42dca9de82fb58b1/s/_/s_baleias_1.jpg',
+      description: 'Baleia'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    }
+  ]
 }
