c6161fd3305539f9246b41b159a8cd252c1e71ea
Marcus Siqueira <marcus.siqueira@serpro.gov.br>
2020-01-29 18:27:11 -0300

    Cria o componente PhotosComponent. Utiliza OnChanges para verificar mudanças em propriedades do componente.

--- web/src/app/photos/photo-list/photo-list.component.ts
+++ web/src/app/photos/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
 
 @Component({
   selector: 'ap-photo-list',
@@ -10,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: Object[] = []
+  photos: Photo[] = []
 
   constructor(
     private photoService: PhotoService,
@@ -21,7 +22,9 @@ export class PhotoListComponent implements OnInit {
     const userName = this.activatedRoute.snapshot.params.userName;
     this.photoService.listFromUser(userName)
       .subscribe(
-        photos => this.photos = photos,
+        photos => {
+          this.photos = photos;
+        },
         err => console.log(err.message)
       );
   }
